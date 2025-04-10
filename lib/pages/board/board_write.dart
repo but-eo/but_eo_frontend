e4c31e54808615b7ca45be29b034af67ff41590b
바카로이 <haeun8520@naver.com>
2025-04-10 17:38:40 +0900

    현재 서버 연결 진행중

---
+++ lib/pages/board/board_write.dart
@@ -0,0 +1,15 @@
+ // 게시글 작성
+
+ import 'package:flutter/cupertino.dart';
+
+class BoardWrite extends StatefulWidget {
+  final String category;
+
+  const BoardWrite({super.key, required this.category});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
--- lib/utils/token_storage.dart
+++ lib/utils/token_storage.dart
@@ -1,11 +1,19 @@
 
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenStorage {
   static Future<void> saveTokens(String accessToken) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString('accessToken', accessToken);
-    //await prefs.setString('userId', userId);
+
+    //JWT 디코딩해서 userId 추출
+    Map<String, dynamic> decodedToken = JwtDecoder.decode(accessToken);
+    String userId = decodedToken['sub'];
+
+    //userId로 저장
+    await prefs.setString('userId', userId);
+    print("userId 저장 : $userId");
   }
 
   static Future<String?> getAccessToken() async {
@@ -13,15 +21,15 @@ class TokenStorage {
     return prefs.getString('accessToken');
   }
 
-  // static Future<String?> getUserId() async {
-  //   final prefs = await SharedPreferences.getInstance();
-  //   return prefs.getString('userId');
-  // }
+  static Future<String?> getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userId');
+  }
 
   static Future<void> clearToken() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove('accessToken');
-    //await prefs.remove('userId');
+    await prefs.remove('userId');
   }
 
 }
