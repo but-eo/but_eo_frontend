fd2f5b3b2531b911648e8171d3eb048de9f98a70
Kingik2 <riplrike8323@gmail.com>
2025-09-30 13:53:19 +0900

    로그인 예외처리 제거

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -362,18 +362,17 @@ class _LoginState extends State<Login> {
                   SizedBox(height: size.height * 0.03),
                   ElevatedButton(
                     onPressed: () async {
-                      // if (_formKey.currentState!.validate()) {
-                      //   _formKey.currentState!.save();
-                      //   print(email);
-                      //   print(password);
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        print(email);
+                        print(password);
 
-                      //   await loginUser(email!, password!);
-                      //   print(loginAuth);
-                      //   if (loginAuth) {
-                      //     navigateToMainPage();
-                      //   }
-                      // }
-                      navigateToMainPage();
+                        await loginUser(email!, password!);
+                        print(loginAuth);
+                        if (loginAuth) {
+                          navigateToMainPage();
+                        }
+                      }
                       
                     },
                     child: Text(
